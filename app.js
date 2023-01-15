f990bebf5de7f403d03bd5ec593dc43923281684
akshaymalik1995 <akshaymalik191@gmail.com>
2023-01-15 21:20:45 +0530

    Added current channel to localStorage

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
-let videos = {}
-let channelChoice = "ted-ed"
 
+
+// This functions accepts a youtube url and return the id of the video
 function getYoutubeId(url){
+    // https://www.youtube.com/watch?v=Fh_UDQnboRw
+    // We find the index of the string "v=" and add 2 to it because from "v=" onwards the next 11 characters consist of the video id
     let startIndex = url.indexOf("v=") + 2
+    // We get the substring starting from the startIndex adding 11 to it because the last character is excluded
     let videoID = url.substring(startIndex, startIndex + 11)
+    // We will get Fh_UDQnboRw
     return videoID
 }
 
-function getRandomNumber(min, max){
+// This function accepts two numbers and it returns a random integer between those two integers 
+function getRandomNumber(min=0, max=Object.keys(VIDEOS).length){
     return Math.floor(Math.random() * (max - min) + min)
 }
 
 
-function embedVideo(startIndex=0 , endIndex=Object.keys(videos).length){
+function embedVideo(){
     const iframe = document.querySelector("iframe")
-    const randomNum = getRandomNumber(startIndex, endIndex)
-    const randomVideo = videos[randomNum]
+    const randomNum = getRandomNumber()
+    const randomVideo = VIDEOS[randomNum]
     const videoId = getYoutubeId(randomVideo.href) 
     iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`
     document.querySelector(".video-title").textContent = `${randomVideo.title}`
     
 }
 
-function changeChannel() {
+function fetchVideos(currentChannelChoice=null) {
     document.querySelector(".loading-text").style.display = 'block'
-    channelChoice = document.querySelector("select").value
+    channelChoice = currentChannelChoice || document.querySelector("select").value
     fetch(`https://akshaymalik1995.github.io/youtube-tv/data/${channelChoice}.json`)
     .then(response => response.json())
-    .then(data => {
+    .then(videos => {
         document.querySelector(".loading-text").style.display = 'none'
-        videos = data
-        let startIndex = 0
-        let endIndex = Object.keys(videos).length
-        embedVideo(startIndex, endIndex)
+        VIDEOS = videos
+        embedVideo()
+        localStorage.setItem("currentChannelChoice" , channelChoice)
     })
     .catch(error => console.log(error))
 }
 
 
 function changeVideo(){
-    startIndex = 0
-    endIndex = Object.keys(videos).length
-    embedVideo(startIndex, endIndex)
+    embedVideo()
 }
 
 
-
-changeChannel()
+let VIDEOS = {}
+let channelChoice = localStorage.getItem("currentChannelChoice") || "ted-ed"
+document.querySelector("select").value = channelChoice
+fetchVideos(channelChoice)
 
 
 
